3d96ebcb587cc103493be2e51a83b5066e076730
hansenjr <Viktor@Viktors-iMac.local>
2016-05-26 00:40:15 +0200

    nytt

---
+++ content/js/script.js
@@ -0,0 +1,17 @@
+var submitBtn = document.getElementById('submitbutt');
+submitBtn.addEventListener("click", validateForm);
+
+function validateForm() {
+  var username = document.userlgn.username.value;
+  var password = document.userlgn.password.value;
+
+  if ((username.trim() == "")) {
+    window.alert("Du måste skriva in din email");
+  }
+  else if ((username.trim() == "") || (username.includes("@") != true) || (username.includes(".") != true)) {
+    window.alert("Du måste skriva in en korrekt mailadress");
+  }
+  else if ((password.trim() == "")) {
+    window.alert("Du fyllde inte i ett lösenord");
+  }
+}
