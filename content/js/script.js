47ed11aa79cd49dbfef9c23b0c8cefdb82ee2689
hansenjr <Viktor@Viktors-iMac.local>
2016-05-26 04:46:15 +0200

    last update for now

--- content/js/script.js
+++ content/js/script.js
@@ -15,3 +15,17 @@ function validateForm() {
     window.alert("Du fyllde inte i ett lösenord");
   }
 }
+
+submitBtn.addEventListener("click", validateReg);
+function validateReg() {
+  var access1 = document.userlvl.access1.checked;
+  var access2 = document.userlvl.access2.checked;
+  var access3 = document.userlvl.access3.checked;
+
+  if ((((access1) == false)) && ((access2) == false) && ((access3) == false)) {
+    window.alert("Du måste ange medlemsnivå");
+  }
+  else {
+    window.location.replace("register-successful.html");
+  }
+}
